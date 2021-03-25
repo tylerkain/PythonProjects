eec32779c88a2c24b4cd96f8f2bb334ec5d0c71a
tylerkain <tyler.kain@outlook.com>
2021-03-25 14:05:39 -0400

    Refactor Code

--- CoffeeMaker/main.py
+++ CoffeeMaker/main.py
@@ -2,24 +2,30 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-menu= Menu()
-menu.get_items()
-
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
 # prompt user
-
-user_drink_choice = input(f" What would you like to drink : {menu.get_items()}: ")
-drink = menu.find_drink(user_drink_choice)
-
+while is_on:
+    user_drink_choice = input(f" What would you like to drink : {menu.get_items()}: ")
 # turn machine off
-if user_drink_choice == "off":
-    print("CoffeeMachine is off")
+    if user_drink_choice == "OFF":
+        is_on: False
+    elif user_drink_choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(user_drink_choice)
+        check_resources = coffee_maker.is_resource_sufficient(drink)
+        payment = money_machine.make_payment(drink.cost)
+        if check_resources and payment:
+            coffee_maker.make_coffee(drink)
 
 # print a report
-coffee_maker = CoffeeMaker()
-if user_drink_choice == "report":
-    print(coffee_maker.report())
 
-# Check Resources sufficient
 
- if coffee_maker.is_resource_sufficient(drink):
-     coffee_maker.make_coffee(drink)
+
+
+# Process Coins
+
