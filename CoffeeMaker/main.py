1978a61029b3ff33e43569b0d8ee3d17c9449290
tylerkain <tyler.kain@outlook.com>
2021-03-25 11:43:51 -0400

    A Coffee Machine Program Using Python and Object-Oriented-Programming

--- CoffeeMaker/main.py
+++ CoffeeMaker/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu= Menu()
+menu.get_items()
+
+# prompt user
+
+user_drink_choice = input(f" What would you like to drink : {menu.get_items()}: ")
+drink = menu.find_drink(user_drink_choice)
+
+# turn machine off
+if user_drink_choice == "off":
+    print("CoffeeMachine is off")
+
+# print a report
+coffee_maker = CoffeeMaker()
+if user_drink_choice == "report":
+    print(coffee_maker.report())
+
+# Check Resources sufficient
+
+ if coffee_maker.is_resource_sufficient(drink):
+     coffee_maker.make_coffee(drink)
