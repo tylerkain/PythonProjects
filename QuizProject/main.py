c88aa353c5dee7342f18d6f222a8061b79bca75f
tylerkain <tyler.kain@outlook.com>
2021-03-26 10:24:11 -0400

    A Quiz Program using Python and OOP

---
+++ QuizProject/main.py
@@ -0,0 +1,12 @@
+from QuestionClass import Question
+from data import question_data
+
+# question = Question()
+# data = question_data()
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
