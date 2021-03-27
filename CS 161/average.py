f86fbd42327066e415968667dff3fbc25da7ce99
tylerkain <tyler.kain@outlook.com>
2021-03-27 12:16:34 -0400

    Python Programs for CS 161

---
+++ CS 161/average.py
@@ -0,0 +1,11 @@
+print("please enter five numbers")
+num_input1 = float(input())
+num_input2 = float(input())
+num_input3 = float(input())
+num_input4 = float(input())
+num_input5 = float(input())
+
+total_sum = num_input1 + num_input2 + num_input3 + num_input4 + num_input5
+avg = total_sum/5
+print("The average of those numbers is: ")
+print(avg)
