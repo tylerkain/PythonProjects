52a2f7e27b39d54f5c55727cc09c8f061139f6fd
tylerkain <68011581+tylerkain@users.noreply.github.com>
2021-03-30 17:14:23 -0400

    A math simulator using the random walk simulation

--- Turtle Graphics/Random_walk.py
+++ Turtle Graphics/Random_walk.py
@@ -2,12 +2,14 @@ from turtle import Turtle, Screen
 import random
 
 tim = Turtle
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]
 directions = [0, 90, 180, 270]
-tim.pensize()
+tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
     tim.color(random.choice(colours))
     tim.forward(30)
     tim.setheading(random.choice(directions))
+
