850e5f0332993c78413c0ec74bb354263ac79ac7
tylerkain <68011581+tylerkain@users.noreply.github.com>
2021-03-30 17:13:43 -0400

    Draws A Spirograph

---
+++ Turtle Graphics/Spirograph.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+########### Challenge 5 - Spirograph ########
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+        tim.speed("fastest")
+
+
+user_choice = int(input("Choose Size: "))
+
+draw_spirograph(user_choice)
+screen = Screen()
+screen.exitonclick()
