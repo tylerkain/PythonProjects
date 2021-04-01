bfb0946ab1ed6346e777cff43529f7300172b04e
tylerkain <68011581+tylerkain@users.noreply.github.com>
2021-04-01 11:53:23 -0400

    Revised Average Program

--- CS 161/average.py
+++ CS 161/average.py
@@ -1,16 +1,24 @@
-import math
+import matplotlib.pyplot as plt
+import pandas as pd
+
 num_grades = int(input(" How many grades would you like to input: "))
 
 
-def grade_input(total_grades,):
+def grade_input(total_grades, ):
     grades = []
     i = 0
     while i < total_grades:
         grade_score = float(input("Enter Grade: "))
         grades.append(grade_score)
         i += 1
-    avg = sum(grades)/i
-    print(f"The average of the test score is: {round(avg,2)} ")
+    avg = sum(grades) / len(grades)
+    plt.plot(grades)
+    print(f"The average of the test score is: {round(avg, 2)} ")
 
 
 grade_input(num_grades)
+grades_df = pd.DataFrame(grade_input(num_grades), columns=['Grades'])
+axes = grades_df.plot(x='Grades', y='Number', style='.-')
+y_label = axes.set_ylabel('Number')
+
+plt.show()
