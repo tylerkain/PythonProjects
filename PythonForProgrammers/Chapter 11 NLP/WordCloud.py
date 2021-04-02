4e860b3a6bef7a4e3070e052c4e08a49433e6632
tylerkain <68011581+tylerkain@users.noreply.github.com>
2021-04-02 12:17:52 -0400

    NLP programs

--- PythonForProgrammers/Chapter 11 NLP/WordCloud.py
+++ PythonForProgrammers/Chapter 11 NLP/WordCloud.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import imageio
 from wordcloud import WordCloud
 
-text = Path('Cell Behaviors.txt').read_text()
+text = Path('Plant Cells.txt').read_text()
 mask_image = imageio.imread('mask_leaf.png')
 
 word_cloud = WordCloud(colormap='prism', mask=mask_image, background_color='white')
 word_cloud = word_cloud.generate(text)
-word_cloud = word_cloud.to_file('CellBehavior.png')
+word_cloud = word_cloud.to_file('PlantCells.png')
