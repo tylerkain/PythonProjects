087a73e653e8cac476002724819c5440f0dcbae3
tylerkain <tyler.kain@outlook.com>
2021-04-24 18:47:11 -0400

    An Etch Sketch App

---
+++ Turtle Graphics/Draw.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def clear_drawing():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_right, 'd')
+screen.onkey(turn_left, 'a')
+screen.onkey(clear_drawing, 'c')
+screen.exitonclick()
